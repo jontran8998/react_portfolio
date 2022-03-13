5a222635c45e6350201d1c22932c94ea1f818ee1
Jon Tran <trungt8998@gmail.com>
2022-03-13 14:59:33 +0700

    complete Skills

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,16 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[
-        images.flutter,
-        images.redux,
-        images.sass,
-        images.graphql,
-        images.javascript,
-        images.python,
-        images.react,
-        images.typescript,
-      ].map((circle, index) => (
+      {[images.sass, images.graphql, images.react].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
