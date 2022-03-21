8d752baf661df77a6f9c321e1c347e47e3ea69f4
Jon Tran <trungt8998@gmail.com>
2022-03-21 12:57:43 +0700

    update header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -58,7 +58,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[images.sass, images.graphql, images.react].map((circle, index) => (
+      {[images.sass, images.react, images.graphql].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
