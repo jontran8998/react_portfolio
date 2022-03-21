bb2b7a495be7a79ae7f0297d11ffa3ea84ccc01d
Jon Tran <trungt8998@gmail.com>
2022-03-21 13:11:50 +0700

    update app wrap font family

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationDots, SocialMedia } from "../components";
 
+import "./AppWrap.scss";
+
 const AppWrap = (Component, idName, classNames) =>
   function HOC() {
     return (
